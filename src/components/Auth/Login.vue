c761c21dd85122e29ddb8ddfbaa9a81aaf4b4b07
Juan <juanchismo10@gmail.com>
2023-03-21 13:35:04 -0300

    Login con vue

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -1,17 +1,58 @@
 <template>
     <div class="log-form">
         <h2>Iniciar sesión</h2>
-        <form>
-            <input type="text" title="username" placeholder="username" />
-            <input type="password" title="username" placeholder="password" />
+        <form @submit.prevent="login">
+            <input type="email" placeholder="email" v-model="email" />
+            <input type="password" placeholder="password" v-model="password" />
             <button type="submit" class="btn">Login</button>
             <a class="forgot" href="#">Forgot Username?</a>
+            <div v-if="err"> {{ err }} </div>
         </form>
     </div><!--end log form -->
 </template>
 <script>
 export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+            err: ''
+        }
+    },
+    methods: {
+        login() {
+            const loginData = {
+                email: this.email,
+                password: this.password
+            }
 
+            fetch('http://127.0.0.1:80/api/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify(loginData)
+            })
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        throw Error('No se pudo autenticar...');
+                    }
+                })
+                .then(data => {
+                    this.email = ''
+                    this.password = ''
+                    this.$router.push('/dashboard')
+                    // console.log(data)
+                })
+                .catch(error => {
+                    this.err = error
+                    // console.log(this.err)
+                })
+        }
+    }
 }
 </script>
 <style></style>
