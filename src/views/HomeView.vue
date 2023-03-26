883f6fb4b7878f5824910d1b80f7dc34a1238547
Juan <juanchismo10@gmail.com>
2023-03-25 22:12:50 -0300

    Corrección de estilos y colores

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup>
 import Home from '../components/Home.vue';
+import Contact from '../components/Contact.vue';
+
 </script>
 
 <template>
   <main>
     <Home />
+    <Contact />
   </main>
 </template>
