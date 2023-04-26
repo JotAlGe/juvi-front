8aac5d2ed7742e6fe846307793f3cfe29a083a12
juanchis <juanchismo10@gmail.com>
2023-04-26 20:49:55 -0300

    listado de mensajes desde el backend

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@ export default {
   data() {
     return {
       isLoggedIn: false,
-      isAdmin: false
+      isAdmin: false,
+      tok: ''
     };
   },
 }
--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -1,12 +1,37 @@
 <template>
     <div>
         <h2>Mensajes</h2>
+        <p v-for="message in messages" :key="message.id">
+            Título: {{ message.title }}
+            <br>
+            Mensaje: {{ message.body }}
+            <hr>
+        </p>
     </div>
 </template>
 <script>
 export default {
 
-
+    data() {
+        return {
+            messages: []
+        }
+    },
+    mounted() {
+        fetch('http://127.0.0.1:8000/api/messages', {
+            headers: {
+                Authorization: `Bearer ${this.$root.tok}`,
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                this.messages = data;
+                console.log(this.messages)
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    },
 }
 </script>
 <style></style>
